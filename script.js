7a0073a7c70da11071763af111f32af86ceeea40
João Victor <joaovictorbp05@gmail.com>
2024-03-11 13:00:50 -0300

    att no comunicado

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const minutos = new Date().getMinutes()
 console.log(minutos)
 const afid = document.querySelector('#afid')
 if (((horas >= 6) && (horas < 12)) || ((horas >= 14) && (horas < 22))) {
-    afid.innerHTML = '<p>UNIDADE <span>ABERTA</span></p>'
+    afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
     afid.classList.add('aberta')
-} else if ((horas >= 12) && (horas <= 13)) {
-    afid.innerHTML = '<p>UNIDADE <span>FECHADA</span> | VOLTAREMOS A FUNCIONAR 14H</p>'
+} else if ((horas >= 12) && (horas < 14)) {
+    afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">14h</span></p>'
     afid.classList.add('fechada')
 } else {
-    afid.innerHTML = '<p>UNIDADE <span>FECHADA</span> | VOLTAREMOS A FUNCIONAR 6H</p>'
+    afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">6h</span></p>'
     afid.classList.add('fechada')
 }
