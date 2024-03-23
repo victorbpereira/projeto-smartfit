f707465a6f35d96db02ecd3463c690dad335f445
João Victor <joaovictorbp05@gmail.com>
2024-03-23 19:51:38 -0300

    comunicado unidade atualizado

--- js/abertura-unidade.js
+++ js/abertura-unidade.js
@@ -1,17 +1,21 @@
 const horas = new Date().getHours()
 const afid = document.querySelector('#afid')
 const diaSemana = new Date().getDay()
-const sabado = diaSemana === 6
+const sabado = (diaSemana === 6)
 
 if (diaSemana < 6) {
     if (((horas >= 6) && (horas < 12)) || ((horas >= 14) && (horas < 22))) {
         afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
         afid.classList.add('aberta')
-    } else {
+    } else if ((horas >= 12) && (horas < 14)) {
         afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">14h</span></p>'
         afid.classList.add('fechada')
+    } else {
+        afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">6h</span></p>'
+        afid.classList.add('fechada')
     }
-} else if (diaSemana === 6) {
+} 
+else if (sabado) {
     if ((horas >= 9) && (horas < 12)) {
         afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
         afid.classList.add('aberta')
