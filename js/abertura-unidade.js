9d2c81a272c5eaa4b610c9b70b71cde8177d1ecf
João Victor <joaovictorbp05@gmail.com>
2024-03-23 19:06:51 -0300

    atualizacao no comunidade

--- js/abertura-unidade.js
+++ js/abertura-unidade.js
@@ -1,12 +1,22 @@
 const horas = new Date().getHours()
 const afid = document.querySelector('#afid')
-if (((horas >= 6) && (horas < 12)) || ((horas >= 14) && (horas < 22))) {
-    afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
-    afid.classList.add('aberta')
-} else if ((horas >= 12) && (horas < 14)) {
-    afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">14h</span></p>'
-    afid.classList.add('fechada')
-} else {
-    afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">6h</span></p>'
-    afid.classList.add('fechada')
+const diaSemana = new Date().getDay()
+const sabado = diaSemana === 6
+
+if (diaSemana < 6) {
+    if (((horas >= 6) && (horas < 12)) || ((horas >= 14) && (horas < 22))) {
+        afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
+        afid.classList.add('aberta')
+    } else {
+        afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR <span class="ahoras">14h</span></p>'
+        afid.classList.add('fechada')
+    }
+} else if (diaSemana === 6) {
+    if ((horas >= 9) && (horas < 12)) {
+        afid.innerHTML = '<p>UNIDADE <span id="a">ABERTA</span></p>'
+        afid.classList.add('aberta')
+    } else {
+        afid.innerHTML = '<p>UNIDADE <span id="f">FECHADA</span> | VOLTAREMOS A FUNCIONAR SEGUNDA-FEIRA <span class="ahoras">6h</span></p>'
+        afid.classList.add('fechada')
+    }
 }
