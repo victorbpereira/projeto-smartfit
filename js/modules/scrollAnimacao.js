4afdaf8393a5fea7d76b4fca9634eb1ae5d11774
João Victor <joaovictorbp05@gmail.com>
2024-03-26 15:14:08 -0300

    animacoes atualizadas

--- js/modules/scrollAnimacao.js
+++ js/modules/scrollAnimacao.js
@@ -3,20 +3,35 @@ export default function animaScroll() {
     const a = document.querySelectorAll('nav a')
     const intro = document.querySelector('.introducao')
     const comunicado = document.querySelector('#afid')
+    const plano = document.querySelectorAll('.plano-bloco')
 
     function aniComunicado() {
         comunicado.classList.add(comunicado.getAttribute('data-animacao'))
     }
+    aniComunicado()
+
     function aniIntro() {
         intro.classList.add(intro.getAttribute('data-animacao'))
     }
+    aniIntro()
+
     function aniNav() {
         a.forEach((i) => {
             i.classList.add(i.dataset.animacao)
         })
     }
-
-    aniComunicado()
     aniNav()
-    aniIntro()
+
+    function aniPlano() {
+        if (window.innerWidth <= 900) {
+            plano.forEach((i) => {
+                i.classList.add(i.dataset.animacao)
+            })
+        } else {
+            plano.forEach((i) => {
+                i.dataset.animacao = ''
+            })
+        }
+    }
+    aniPlano()
 }
